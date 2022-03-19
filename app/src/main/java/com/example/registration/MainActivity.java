be539328c78a2cd67f622693916ee182c468f61d
dwityamaudyalfira <76533748+dwityamaudyalfira@users.noreply.github.com>
2022-03-19 09:08:30 +0700

    Registration dan Contacts

---
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_user);
+    }
+
+}
